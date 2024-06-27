585f893052a109728ef23cb3ba09d907abda8e97
丁俊杰 <10725046+JunJD@user.noreply.gitee.com>
2024-06-27 22:26:24 +0800

    feat： serve  update

--- src/app/api/getBatteryInfo/route.ts
+++ src/app/api/getBatteryInfo/route.ts
@@ -1,42 +1,27 @@
 import { NextResponse } from "next/server"
 
-export const maxDuration = 60
-
 export const POST = async function (req: Request) {
-    if (req.method === "OPTIONS") {
-        return NextResponse.next()
-    }
-    const { token, dcbh } = await req.json()
-    const response = await fetch(`
-    https://jgjfjdcgl.gat.zj.gov.cn:5102/inf_zpm/hz_mysql_api/BatteryBinding/dcinfoquery?token=${token}&dcbhurl=https://www.pzcode.cn/pwb/${dcbh}
-    `, {
-        method: "GET",
-        headers: {
-            authority: 'jgjfjdcgl.gat.zj.gov.cn:5102',
-            scheme: 'https',
-            'accept-encoding': 'gzip',
-            'user-agent': "okhttp/4.9.3"
+    try {
+        if(req.method==="OPTIONS") {
+            return NextResponse.next()
         }
-    })
-    const data = await response.json()
-
-    if (data.code === 0) {
-        const response2 = await fetch(`https://www.pzcode.cn/pwb/${dcbh}`, {
-            redirect: 'follow',
-            // 超时时间
+        const { token, dcbhurl } = await req.json()
+        const response = await fetch(`
+        https://jgjfjdcgl.gat.zj.gov.cn:5102/inf_zpm/hz_mysql_api/BatteryBinding/dcinfoquery?token=${token}&dcbhurl=${dcbhurl}
+        `, {
+            method: "GET",
+            headers: {
+                authority: 'jgjfjdcgl.gat.zj.gov.cn:5102',
+                scheme: 'https',
+                'accept-encoding': 'gzip',
+                'user-agent': "okhttp/4.9.3"
+            }
         })
-        const text = await response2.text();
-        const 销售单位未入库 = text.includes('销售单位未入库')
-
-        if (销售单位未入库) {
-            return NextResponse.json({
-                ...data, code: 6, url: `
-            https://jgjfjdcgl.gat.zj.gov.cn:5102/inf_zpm/hz_mysql_api/BatteryBinding/dcinfoquery?token=${token}&dcbhurl=https://www.pzcode.cn/pwb/${dcbh}`
-            }, { status: 200 })
-        }
+        const data = await response.json()
+    
+        return NextResponse.json({ ...data, url: `https://jgjfjdcgl.gat.zj.gov.cn:5102/inf_zpm/hz_mysql_api/BatteryBinding/dcinfoquery?token=${token}&dcbhurl=${dcbhurl}` }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ code: 1 }, { status: 200 })
+        
     }
-    return NextResponse.json({
-        ...data, url: `
-    https://jgjfjdcgl.gat.zj.gov.cn:5102/inf_zpm/hz_mysql_api/BatteryBinding/dcinfoquery?token=${token}&dcbhurl=https://www.pzcode.cn/pwb/${dcbh}`
-    }, { status: 200 })
 }
